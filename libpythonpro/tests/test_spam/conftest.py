bf6337eff52d60838a694ddaa680107ff988b00c
jarlan.silva <silvajarlan@gmail.com>
2021-06-30 17:36:33 -0300

    Pytest Fixtures correção flake8

--- libpythonpro/tests/test_spam/conftest.py
+++ libpythonpro/tests/test_spam/conftest.py
@@ -6,7 +6,7 @@ from libpythonpro.spam.db import Conexao
 @pytest.fixture(scope='session')
 def conexao():
     # Setup
-    conexao_obj =  Conexao()
+    conexao_obj = Conexao()
     yield conexao_obj
     # Tear Down
     conexao_obj.fechar()
